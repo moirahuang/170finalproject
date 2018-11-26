9a418005f846a17a4d333294c471300a011e5eb1
moirahuang <huang.moira@gmail.com>
2018-11-26 12:32:33 -0800

    comments

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import cvxpy
 import numpy as np
+
+# construct a dictionary mapping each person to the rowdy groups they're in
+
+# iterate through every person
+
+# iterate through the buses and construct an array with the number of friends they have on the buses
+# ILP to pick the bus with the max heuristic (based on vectors)
+# heuristic is # of friendships created - factor * potential rowdy groups created (but make -infinity if a rowdy group is created)
+
+# simulated anealing to swap for best solution
