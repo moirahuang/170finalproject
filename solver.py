097da9f7c59eea73917410c41924ba2a4fe74174
moirahuang <huang.moira@gmail.com>
2018-11-27 14:32:45 -0800

    comments

--- solver.py
+++ solver.py
@@ -46,6 +46,17 @@ def parse_input(folder_name):
 
 def solve():
     #TODO: Write this method as you like. We'd recommend changing the arguments here as well
+
+    # construct a dictionary mapping each person to the rowdy groups they're in
+
+    # iterate through every person
+
+    # iterate through the buses and construct an array with the number of friends they have on the buses
+    # ILP to pick the bus with the max heuristic (based on vectors)
+    # heuristic is # of friendships created - factor * potential rowdy groups created (but make -large value if a rowdy group is created--recalculate friendships if a rowdy group has to be made)
+
+    # simulated anealing to swap for best solution
+
     pass
 
 def main():
