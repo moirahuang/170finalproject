df27e612c6ae3309e636fccce202f18604a4e7dc
EC2 Default User <ec2-user@ip-172-31-69-253.ec2.internal>
2018-12-03 05:09:37 +0000

    aws

--- simann.py
+++ simann.py
@@ -355,7 +355,7 @@ def main():
         the portion which writes it to a file to make sure their output is
         formatted correctly.
     '''
-    size_categories = ["small"]
+    size_categories = ["small", "medium", "large"]
     if not os.path.isdir(path_to_outputs):
         os.mkdir(path_to_outputs)
 
